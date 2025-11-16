9d8345c4f9c8d644e6a83799c99376a88b76a3df
Joshua / Catherine Moylan <35865600+DatSonicBoom@users.noreply.github.com>
2025-11-16 22:33:15 +1100

    Add null argument check to ConstantPoolItem constructor

--- src/class_file_creator/constant_pool/ConstantPoolItem.java
+++ src/class_file_creator/constant_pool/ConstantPoolItem.java
@@ -5,7 +5,11 @@ public abstract class ConstantPoolItem {
     protected final ConstantPool constantPool;
     protected final int slot;
 
-    protected ConstantPoolItem(ConstantPool constantPool, int slot) {
+    protected ConstantPoolItem(ConstantPool constantPool, int slot) throws IllegalArgumentException {
+
+        if (constantPool == null)
+            throw new IllegalArgumentException("constantPool cannot be null");
+
         this.constantPool = constantPool;
         this.slot = slot;
     }

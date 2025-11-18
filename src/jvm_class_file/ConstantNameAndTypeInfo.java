d161baceb4e872b547adb5ac24cd5ff64f542cf1
Joshua / Catherine Moylan <35865600+DatSonicBoom@users.noreply.github.com>
2025-11-18 18:10:20 +1100

    Remove equals() and hashCode() implementations in ConstantPoolEntry subclasses

--- src/jvm_class_file/ConstantNameAndTypeInfo.java
+++ src/jvm_class_file/ConstantNameAndTypeInfo.java
@@ -30,29 +30,7 @@ public class ConstantNameAndTypeInfo extends ConstantPoolEntry {
     protected void write(DataOutputStream dos) throws IOException {
 
         dos.writeByte(12); // Write tag (always 12 for CONSTANT_NameAndType_info)
-        dos.writeShort(this.name.index()); // Write name_index
-        dos.writeShort(this.descriptor.index()); // Write descriptor_index
-    }
-
-    @Override
-    public int hashCode() {
-        return (this.name.index << 2) | this.descriptor.index;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-
-        if (o instanceof ConstantNameAndTypeInfo c) {
-
-            if (this.jvmClassFile != c.jvmClassFile)
-                return false;
-
-            if (!this.name.equals(c.name))
-                return false;
-
-            return this.descriptor.equals(c.descriptor);
-        }
-
-        return false;
+        dos.writeShort(this.name.index); // Write name_index
+        dos.writeShort(this.descriptor.index); // Write descriptor_index
     }
 }
